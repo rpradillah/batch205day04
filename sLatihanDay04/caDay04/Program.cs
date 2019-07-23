5e29211c0c28c566faea85435c90fcfc4b62e1bc
Rizkiananda Pradillah <rpradillah@gmail.com>
2019-07-23 14:51:19 +0700

    first commit

---
+++ sLatihanDay04/caDay04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay04
+{
+    class Program
+    {
+        public static int count(int x)
+        {
+            return x + 2;
+        }
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int number2 = count(number);
+            bool status = true;
+            //penulisan dengan parameter index
+            Console.WriteLine("Welcome Back ! {0} {1}", number,status);
+            //penulisan dengan ++ 
+            Console.WriteLine("Welcome Back ! " + number + " " + status);
+            Console.WriteLine(number2);
+        }
+    }
+}
---
+++ sLatihanDay04/simpleArraySum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleArraySum
+{
+    class Program
+    {
+    static int simpleArraySum(int[] arCount) {
+            int sum = 0;
+            for (int i = 0; i < arCount.Length; i++)
+            {
+                sum += Convert.ToInt32(arCount[i]);
+            }
+            return sum;
+    }
+    static void Main(string[] args) {
+        int arCount = Convert.ToInt32(Console.ReadLine());
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int result = simpleArraySum(ar);
+        Console.WriteLine(result);
+    }
+    }
+}
